06e523fbb88d24f00fd86f74616c8fb2e5ade678
Ricardo Fearing <9965014+rfearing@users.noreply.github.com>
2022-03-17 06:59:35 -0400

    Fix bugs found from old computer

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,2 +1,2 @@
-ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
-ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application Support/Code/User/keybindings.json
+ln -s ~/.dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/.dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
