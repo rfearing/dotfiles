7c80bb69f97f27ec15024208badcf70cffc4eb2d
Ricardo Fearing <rfearing@github.com>
2022-06-23 17:15:09 -0400

    Update Bash to install on linux

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -6,11 +6,19 @@
 # using Homebrew.
 
 # Check for Homebrew
-if test ! $(which brew) && test "$(uname)" = "Darwin"
+if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
 
 fi
+
 exit 0
