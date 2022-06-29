d9555e9500daca001fae02e7848ec7682288c5fe
Ricardo Fearing <rfearing@github.com>
2022-06-29 10:22:37 -0400

    Wrap chruby for osx

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,5 +1,7 @@
-# Load chruby every time you open a terminal
+if [[ $OSTYPE == 'darwin'* ]]; then
+	# Load chruby every time you open a terminal
 
-source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
-source /opt/homebrew/opt/chruby/share/chruby/auto.sh
-chruby ruby-3.1.2
+	source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+	source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+	chruby ruby-3.1.2
+fi
