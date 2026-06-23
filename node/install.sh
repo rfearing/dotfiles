ca1785674963864d73bf4c582b28d8b8ee78cd1c
rfearing <rfearing@github.com>
2026-06-23 13:09:55 -0400

    Updates from the new computer

--- node/install.sh
+++ node/install.sh
@@ -9,5 +9,5 @@ then
 fi
 
 if [ ! -d "$NVM_DIR" ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash --no-use
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.5/install.sh | bash --no-use
 fi
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,24 +2,9 @@
 if [[ $OSTYPE == 'darwin'* ]]; then
   # Set PHP, Ruby and Node Version for iTerm2
   function iterm2_print_user_vars() {
-    iterm2_set_user_var phpVersion $(php -v | awk '/^PHP/ { print $2 }')
-    iterm2_set_user_var rubyVersion $(ruby -v | awk '{ print $2 }')
+    # iterm2_set_user_var phpVersion $(php -v | awk '/^PHP/ { print $2 }')
+    # iterm2_set_user_var rubyVersion $(ruby -v | awk '{ print $2 }')
     iterm2_set_user_var nodeVersion $(node -v)
-  }
-
-  # Play Lofi Girl to focus
-  function lofi () {
-    mpv "https://www.youtube.com/watch?v=jfKfPfyJRdk" --vid=no
-  }
-
-  # play yt in terminal with no video
-  function listen () {
-    mpv $1 --vid=no
-  }
-
-  # play yt in terminal with video
-  function watch () {
-    mpv $1
   }
 
   # Print out current wifi password
